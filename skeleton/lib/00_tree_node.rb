0fbf0edd4bf3c25506773917a84fdf6c4c35a37a
App Academy Student <alexandraliao3511@gmail.com>
2019-06-14 14:00:27 -0400

    Add #parent=, #add_child, #remove_child

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class PolyTreeNode
 
   def initialize(value)
@@ -18,9 +20,31 @@ class PolyTreeNode
     @value
   end
 
-  # def parent=(node)
-  #   self.parent = node
-  #   node.children << self
-  # end
+  def parent=(node)
+    @parent.children.delete(self) unless @parent.nil?
+    @parent = node
+    unless node.nil? || node.children.include?(self)
+      node.children << self
+    end
+  end
+
+  def add_child(child_node)
+    child_node.parent = self
+  end
+
+  def remove_child(child_node)
+    raise "Idiot! Node not child" unless self.children.include?(child_node)
+    child_node.parent = nil
+  end
+
+  def dfs(target_value)
+    return self if self.value == target_value
+    self.children.each do |child|
+      child.dfs(target_value)
+    end
+  end
+
+  def bfs(target_value)
+  end
 
 end
